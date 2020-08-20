09ead66f7453360f4d90250ea1f8dda9a287e217
Joshua Pukin <joshuapukin@Joshuas-MBP-2.fios-router.home>
2020-08-20 16:19:44 -0400

    feat(gui) for terminal added and started on automated statusOutput

--- ui.js
+++ ui.js
@@ -1,20 +1,23 @@
 'use strict';
 const React = require('react');
-const {Box} = require('ink');
+const {Text, Box, Spacer} = require('ink');
+const statusOutput = require('./testChildProcess')
 
-const App = ({name = 'Stranger'}) => (
-	<Box borderStyle="round" borderColor="green" className="full-app">
+const App = () => (
+	<Box borderStyle="classic" borderColor="green" className="full-app" height='100%' >
 
-		<Box borderStyle="round" borderColor="green" className="left-box" width="50%" flexDirection="column">
-			<Box className="changed-files" borderStyle="round" borderColor="green" height="50%">
-
+		<Box borderStyle="round" borderColor="red" className="left-box" width="50%" flexDirection="column">
+			<Box className="changed-files" borderStyle="round" borderColor="white" height="50%">
+				<Text status={statusOutput}>Here: {statusOutput}</Text>
 			</Box>
-			<Box className="stage-area" borderStyle="round" borderColor="green" height="50%">
-
+			<Box className="stage-area" borderStyle="round" borderColor="white" height="50%">
+				<Text>
+					Staged-Area
+				</Text>
 			</Box>
 		</Box>
-		<Box borderStyle="round" borderColor="green" className="left-box" width="50%">
-
+		<Box className='gitBranch' borderStyle="round" borderColor="red" className="left-box" width="50%">
+			<Text>Git Branch</Text>
 		</Box>
 
 	</Box>
