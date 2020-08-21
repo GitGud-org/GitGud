52b8c378c253de2a45a3c598ccf38573b85663a8
Robert Harkov <robertharkov@gmail.com>
2020-08-21 17:28:43 -0400

    tab bug

--- testChildProcess.js
+++ testChildProcess.js
@@ -1,18 +1,30 @@
-const { execSync } = require('child_process');
-const { stat } = require('fs');
+const { execSync } = require("child_process");
+const { stat } = require("fs");
 
 const statusOutput = () => {
-    let gitStatusOutput = execSync('git status', (error, stdout, stderr) => {
-        if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-        }
-        return stdout
-      })
-      .toString()
-			// console.log('test', gitStatusOutput)
-			return gitStatusOutput
-}
+	let gitStatusOutput = execSync("git status", (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return;
+		}
+
+		return stdout;
+	})
+		.toString()
+		.split(" ");
+	// console.log("test", gitStatusOutput);
+	let branch = gitStatusOutput[2];
+	// console.log(branch);
+	let changedFiles = gitStatusOutput.filter((elem) =>
+		elem.includes("modified")
+	);
+	changedFiles = changedFiles.map((elem) =>
+		elem.replace("modified", "").replace(/(\t:)/gm, "")
+	);
+	changedFiles.shift();
+
+	console.log(changedFiles);
+	return changedFiles;
+};
 
 module.exports = statusOutput;
-
--- ui.js
+++ ui.js
@@ -1,34 +1,62 @@
-'use strict';
-const React = require('react');
-const {useEffect, useState} = require('react');
-const {Text, Box, Spacer} = require('ink');
-const statusOutput = require('./testChildProcess')
+"use strict";
+const React = require("react");
+const { useEffect, useState } = require("react");
+const { Text, Box, Spacer, Newline } = require("ink");
+const statusOutput = require("./testChildProcess");
 
 const App = () => {
-	const [status, setStatus] = useState('');
+	const [status, setStatus] = useState("");
 
 	useEffect(() => {
-		setStatus(statusOutput())
-	}, [])
+		setStatus(statusOutput());
+	}, []);
 
-	return(
-	<Box borderStyle="classic" borderColor="green" className="full-app" height='100%' >
-
-		<Box borderStyle="round" borderColor="red" className="left-box" width="50%" flexDirection="column">
-			<Box className="changed-files" borderStyle="round" borderColor="white" height="80%">
-				<Text >Here: {status}</Text>
+	return (
+		<Box
+			borderStyle="classic"
+			borderColor="green"
+			className="full-app"
+			height="100%"
+		>
+			<Box
+				borderStyle="round"
+				borderColor="red"
+				className="left-box"
+				width="50%"
+				flexDirection="column"
+			>
+				<Box
+					className="changed-files"
+					borderStyle="round"
+					borderColor="white"
+					height="100%"
+				>
+					<Box height="100%">
+						<Text>
+							Modified files: <Newline /> {status}
+						</Text>
+					</Box>
+				</Box>
+				<Box
+					className="stage-area"
+					borderStyle="round"
+					borderColor="white"
+					height="100%"
+				>
+					<Text>Staged-Area</Text>
+				</Box>
 			</Box>
-			<Box className="stage-area" borderStyle="round" borderColor="white" height="100%">
-				<Text>
-					Staged-Area
-				</Text>
+			<Box
+				className="gitBranch"
+				borderStyle="round"
+				borderColor="red"
+				className="left-box"
+				width="50%"
+			>
+				<Text>Git Branch</Text>
 			</Box>
 		</Box>
-		<Box className='gitBranch' borderStyle="round" borderColor="red" className="left-box" width="50%">
-			<Text>Git Branch</Text>
-		</Box>
-
-	</Box>
-)};
+	);
+};
 
 module.exports = App;
