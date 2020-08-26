cff93d130fba6d1eb8315b2486723853d4640715
Angela Ratto <angelaratto@gmail.com>
2020-08-26 16:56:37 -0400

    interim changes

--- Selector.js
+++ Selector.js
@@ -1,8 +1,12 @@
 const React = require("react");
 const SelectInput = require("ink-select-input-horizontal").default;
+const { Text, Box, Newline, Spacer } = require("ink");
 const { propTypes } = require("ink-gradient");
+const importJsx = require("import-jsx")
 
-const stageFiles = require('./actions/stageFiles')
+const FileStageMultiSelector = importJsx('./components/FileStageMultiSelector')
+
+const stageTab = require('./actions/stageFiles')
 const pushTab = require('./actions/pushTab')
 const revertTab = require('./actions/revertStaged');
 const pullTab = require('./actions/pullBranch')
@@ -10,7 +14,7 @@ const pullTab = require('./actions/pullBranch')
 const Selector = () => {
 	const handleSelect = (item) => {
 		// `item` = { label: 'First', value: 'first' }
-		if(item === items[0]) {stageFiles()}
+		if(item === items[0]) {stageTab()}
 		if(item === items[1]){revertTab()}
 		if(item === items[2]){pushTab()}
 		if(item === items[3]){pullTab()}
@@ -35,7 +39,12 @@ const Selector = () => {
 		},
 	];
 
-	return <SelectInput items={items} onSelect={handleSelect} />;
+	return (
+		<Box>
+			<SelectInput items={items} onSelect={handleSelect} />
+			<FileStageMultiSelector />
+		</Box>
+	);
 };
 
 module.exports = Selector;
