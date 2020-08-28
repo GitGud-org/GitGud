803bef57858c188622a59709ad33e1190f7a0f93
Angela Ratto <angelaratto@gmail.com>
2020-08-28 12:50:57 -0400

    puts git tree command call as string on state and only processes if it needs to re-render

---
+++ actions/gitBranchVisualPull.js
@@ -0,0 +1,24 @@
+const { execSync } = require('child_process')
+
+const gitBranchVisualPull = () => {
+
+	let gitBranchVisual = execSync(
+			//runs the branch visual command
+			//useful git snippets to maybe implement later down the line;
+					//git shortlog
+					//git log --oneline -(to be used for bottom of git branch)
+			// 'git log --all --decorate --oneline --graph',
+			'git log --pretty=format:"%s" --graph --decorate', //temporarily added the %s back in
+			(error, stdout, stderr) => {
+					if(error) {
+							console.error(`exec errorL ${error}`)
+							return
+					}
+					return stdout
+			}
+	).toString()
+
+	return gitBranchVisual
+}
+
+module.exports = gitBranchVisualPull
