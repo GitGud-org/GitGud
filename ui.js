28919142f452e45a0814ea00e41ce1a178dd4671
Angela Ratto <angelaratto@gmail.com>
2020-09-01 15:45:08 -0400

    adds styleFile file which allows changing of showLogo, defaultColor, and accentColor. Does not currently impact added components like Selector as they don't have color options. Can be done wiht modifications to packages.

--- ui.js
+++ ui.js
@@ -5,13 +5,14 @@ const { useEffect, useState, useRef } = require("react");
 const { Text, Box, measureElement, Newline, Spacer } = require("ink");
 
 const Renderer = require("./components/Divider");
-const gitBranchCall = require("./currentBranch");
 const Gradient = require("ink-gradient");
 const BigText = require("ink-big-text");
 
 const importJsx = require("import-jsx");
 const Selector = importJsx("./Selector.js");
+const Logo = importJsx("./components/Logo")
 
+const gitBranchCall = require("./currentBranch");
 const gitStatusPull = require('./actions/gitStatusPull')
 const gitStatusProcess = require('./actions/gitStatusProcess')
 const gitBranchVisualPull = require('./actions/gitBranchVisualPull')
@@ -19,6 +20,8 @@ const gitBranchVisualProcess = require('./actions/gitBranchVisualProcess')
 
 const enterAltScreenCommand = "\x1b[?1049h";
 const leaveAltScreenCommand = "\x1b[?1049l";
+
+const {showLogo, defaultColor, accentColor} = require('./styleFile')
 
 const exitFullScreen = () => {
 	process.stdout.write(leaveAltScreenCommand);
@@ -56,14 +59,10 @@ const App = () => {
 
 	return (
 		<Box flexDirection="column">
-			<Box justifyContent="center">
-				<Gradient name="morning">
-					<BigText text="GITGUD" />
-				</Gradient>
-			</Box>
+			{showLogo && <Logo />}
 			<Box
 				borderStyle="round"
-				borderColor="red"
+				borderColor={accentColor}
 				className="full-app"
 				height={20}
 				flexGrow={1}
@@ -79,17 +78,14 @@ const App = () => {
 					<Box className="changed-files" height="50%">
 						<Box height="100%">
 							<Box flexDirection="column" alignItems="flex-start">
-								<Text color="red" bold underline>
+								<Text color={accentColor} bold underline>
 									Unstaged Changes
 								</Text>
-								{/* <Newline /> */}
-								{/* <Box flexDirection="column" alignItems="flex-start"> */}
-									{statusProcessed.unstaged.map(file => <Box alignItems="flex-start" key={file}><Text>{file}</Text></Box>)}
-								{/* </Box> */}
+									{statusProcessed.unstaged.map(file => <Box alignItems="flex-start" key={file}><Text color={defaultColor}>{file}</Text></Box>)}
 							</Box>
 						</Box>
 					</Box>
-					<Text color="red">
+					<Text color={accentColor}>
 						<Newline />
 						<Renderer width={appWidth} />
 					</Text>
@@ -97,11 +93,11 @@ const App = () => {
 					<Box className="stage-area" height="50%">
 						<Box height="100%">
 							<Box flexDirection="column" alignItems="flex-start">
-								<Text color="red" bold underline>
+								<Text color={accentColor} bold underline>
 									Staged Changes
 								</Text>
 								{/* <Newline /> */}
-								{statusProcessed.staged.map(file => <Box alignItems="flex-start" key={file}><Text>{file}</Text></Box>)}
+								{statusProcessed.staged.map(file => <Box alignItems="flex-start" key={file}><Text color={defaultColor}>{file}</Text></Box>)}
 							</Box>
 						</Box>
 					</Box>
@@ -109,31 +105,29 @@ const App = () => {
 				<Box
 					className="gitBranch"
 					borderStyle="round"
-					borderColor="red"
+					borderColor={accentColor}
 					className="left-box"
 					width="65%"
 					margin="-1"
 					flexDirection="column"
 				>
 					<Box flexDirection="row">
-						<Text color="red" bold underline>
+						<Text color={accentColor} bold underline>
 							Git Branch --{">"}
 						</Text>
-						<Text> {branch}</Text>
+						<Text color={defaultColor}> {branch}</Text>
 						<Spacer />
-						<Text>Newest to Oldest </Text>
+						<Text color={defaultColor}>Newest to Oldest </Text>
 					</Box>
 					<Box flexDirection="row">
-						{/* <Text color="green">{visual.astrix}</Text> */}
-						<Text color="white" bold>
+						<Text color={defaultColor} bold>
 							{visualProcessed.sorted}
 						</Text>
 						<Text> </Text>
-						{/* <Text color='white'>{text.sorted}</Text>  */}
 					</Box>
 				</Box>
 			</Box>
-			<Selector />
+			<Selector defaultColor={defaultColor} accentColor={accentColor} />
 		</Box>
 	);
 };
