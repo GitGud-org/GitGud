4464267d0657baa7458064f3981d7530aface1d5
Joshua Pukin <joshuapukin@gmail.com>
2020-09-02 18:29:35 -0400

    added new package and tweaked for color changes

--- actions/stash.js
+++ actions/stash.js
@@ -1,7 +1,7 @@
 const React = require('react')
 const { useEffect, useState } = require('react')
 const { Text, Box, Newline } = require('ink')
-const SelectInput = require('ink-select-input').default
+const SelectInput = require('ink-select-input-horizontal').default
 const { execSync, exec } = require('child_process')
 
 //stash functionality
