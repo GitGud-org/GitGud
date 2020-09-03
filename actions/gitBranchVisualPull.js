36bb42123244e44953f06c968f01d374371c2f21
Angela Ratto <angelaratto@gmail.com>
2020-09-03 10:38:04 -0400

    adds try-catch to gitBranchVisualPull. does not error out if no tree.

--- actions/gitBranchVisualPull.js
+++ actions/gitBranchVisualPull.js
@@ -1,25 +1,31 @@
 const { execSync } = require('child_process')
 
 const gitBranchVisualPull = () => {
+	let gitBranchVisualPull = ''
+	try {
 
-	let gitBranchVisual = execSync(
+		gitBranchVisual = execSync(
 			//runs the branch visual command
 			//useful git snippets to maybe implement later down the line;
 			//git shortlog
 			//git log --oneline -(to be used for bottom of git branch)
 			// 'git log --all --decorate --oneline --graph',
 			//temporarily added the %s back in
-			'git log --pretty=format:"%s" --graph --decorate', 
-			(error, stdout, stderr) => {
-					if(error) {
-							console.error(`exec errorL ${error}`)
-							return
-					}
-					return stdout
-			}
-	).toString()
+			'git log --pretty=format:"%s" --graph --decorate',
+			// (error, stdout, stderr) => {
+			// 	if(error) {
+			// 		// console.error(`exec errorL ${error}`)
+			// 		return
+			// 	}
+			// 	// return stdout
+			// 	return
+			// }
+			).toString()
 
-	return gitBranchVisual
+		} catch (error) {
+			return ''
+		}
+		return gitBranchVisual
 }
 
 module.exports = gitBranchVisualPull
