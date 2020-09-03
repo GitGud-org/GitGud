bbedbef77d214ef595034cb8b624c44f4be66cf7
Angela Ratto <angelaratto@gmail.com>
2020-09-03 10:39:41 -0400

    code cleanup

--- actions/gitBranchVisualPull.js
+++ actions/gitBranchVisualPull.js
@@ -11,15 +11,7 @@ const gitBranchVisualPull = () => {
 			//git log --oneline -(to be used for bottom of git branch)
 			// 'git log --all --decorate --oneline --graph',
 			//temporarily added the %s back in
-			'git log --pretty=format:"%s" --graph --decorate',
-			// (error, stdout, stderr) => {
-			// 	if(error) {
-			// 		// console.error(`exec errorL ${error}`)
-			// 		return
-			// 	}
-			// 	// return stdout
-			// 	return
-			// }
+			'git log --pretty=format:"%s" --graph --decorate'
 			).toString()
 
 		} catch (error) {
