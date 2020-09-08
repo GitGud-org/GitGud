34160940a3d0e40cd0bcaddbfa67cc6fe53f474b
Joshua Pukin <joshuapukin@gmail.com>
2020-09-08 14:42:25 -0400

    commit

--- styleFile.js
+++ styleFile.js
@@ -1,9 +1,9 @@
 module.exports = {
 	// setting to show or not show GitGud logo at top of app
-	showLogo: false, // accepted values include [true, false]
+	showLogo: true, // accepted values include [true, false]
 
 	// setting for default text colors
-	defaultColor: 'green', // accepted values include ['red', 'blue', 'white', 'green', etc.]
+	defaultColor: 'white', // accepted values include ['red', 'blue', 'white', 'green', etc.]
 
 	// setting for accent text and line colors
 	accentColor: 'red', // accepted values include ['red', 'blue', 'white', 'green', etc.]
